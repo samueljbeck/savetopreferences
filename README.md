115c3dfa7a4482332c1b498b74d4b9d0bc426209
Samuel Beck <sam@acst.com>
2018-12-11 13:12:51 -0800

    update to readme

--- README.md
+++ README.md
@@ -1,5 +1,61 @@
 # savetopreferences
 Simple Library for adding and removing values from preferences
 
+```
+com.sameljbeck.utilities:savetopreferences:0.0.3
+
+```
+
+### Java Use:
+
+``` 
+###set value
+new PreferenceSave().set(context, "key", "value")
+
+###get value
+new PreferenceSave().get(context, "key")
+
+###getIfContains. returns a default value if the value is not in preferences
+new PreferenceSave().get(context, "key", "value")
+
+###clear value
+new PreferencesSave().clear(context, "key")
+
+###contains checks if a key is present
+new PreferenceSave().contains(context, "key")
+
+###setBool saves boolean as string
+new PreferenceSave().setBool(context, "key", boolean)
+
+###getBool gets string as boolean. //defaults to false
+new PreferenceSave().getBool(context, "key")
+```
+
+###Kotlin Use
+``` 
+###set value
+PreferenceSave().set(context, "key", "value")
+
+###get value
+PreferenceSave().get(context, "key")
+
+###getIfContains. returns a default value if the value is not in preferences
+PreferenceSave().get(context, "key", "value")
+
+###clear value
+PreferencesSave().clear(context, "key")
+
+###contains checks if a key is present
+PreferenceSave().contains(context, "key")
+
+###setBool saves boolean as string
+PreferenceSave().setBool(context, "key", boolean)
+
+###getBool gets string as boolean. //defaults to false
+PreferenceSave().getBool(context, "key")
+```
+
+
+
 
 
--- src/main/java/com/samueljbeck/utilities/savetopreferences/PreferenceSave.kt
+++ src/main/java/com/samueljbeck/utilities/savetopreferences/PreferenceSave.kt
@@ -6,24 +6,13 @@ import android.content.Context
  * Created by Sam Beck on 10/5/2018
  */
 class PreferenceSave {
-    companion object {
 
-        private const val TOP_KEY = "com.samueljbeck.top.key"
+    private val TOP_KEY = "com.samueljbeck.top.key"
 
-        operator fun set(context: Context, key: String, value: String?) {
-            if (value == null) {
-                clear(context, key)
-            } else {
-                val sharedPreferences = context.getSharedPreferences(
-                        TOP_KEY + key,
-                        Context.MODE_PRIVATE)
-                val editor = sharedPreferences.edit()
-                editor.putString(key, value)
-                editor.apply()
-            }
-        }
-
-        fun setSync(context: Context, key: String, value: String) {
+    operator fun set(context: Context, key: String, value: String?) {
+        if (value == null) {
+            clear(context, key)
+        } else {
             val sharedPreferences = context.getSharedPreferences(
                     TOP_KEY + key,
                     Context.MODE_PRIVATE)
@@ -31,47 +20,60 @@ class PreferenceSave {
             editor.putString(key, value)
             editor.apply()
         }
+    }
 
-        operator fun get(context: Context, key: String): String? {
+    fun setSync(context: Context, key: String, value: String) {
+        val sharedPreferences = context.getSharedPreferences(
+                TOP_KEY + key,
+                Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString(key, value)
+        editor.apply()
+    }
 
-            val sharedPreferences = context.getSharedPreferences(
-                    TOP_KEY + key,
-                    Context.MODE_PRIVATE)
-            return sharedPreferences.getString(key, "")
+    operator fun get(context: Context, key: String): String? {
+
+        val sharedPreferences = context.getSharedPreferences(
+                TOP_KEY + key,
+                Context.MODE_PRIVATE)
+        return sharedPreferences.getString(key, "")
+    }
+
+    fun getIfContains(context: Context, key: String, value: String): String {
+        return if (contains(context, key)) {
+            get(context, TOP_KEY + key)!!
+        } else {
+            value
         }
+    }
 
-        fun getIfContains(context: Context, key: String, value: String): String {
-            return if (contains(context, key)) {
-                get(context, key)!!
-            } else {
-                value
-            }
-        }
+    fun clear(context: Context, key: String) {
 
-        fun clear(context: Context, key: String) {
+        val sharedPreferences = context.getSharedPreferences(
+                TOP_KEY + key,
+                Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.remove(key)
+        editor.apply()
+    }
 
-            val sharedPreferences = context.getSharedPreferences(
-                    TOP_KEY + key,
-                    Context.MODE_PRIVATE)
-            val editor = sharedPreferences.edit()
-            editor.remove(key)
-            editor.apply()
-        }
+    fun contains(context: Context, key: String): Boolean {
+        val sharedPreferences = context.getSharedPreferences(
+                TOP_KEY + key,
+                Context.MODE_PRIVATE)
+        return sharedPreferences.contains(key)
 
-        fun contains(context: Context, key: String): Boolean {
-            val sharedPreferences = context.getSharedPreferences(
-                    TOP_KEY + key,
-                    Context.MODE_PRIVATE)
-            return sharedPreferences.contains(key)
+    }
 
-        }
+    fun setBool(context: Context, key: String, value: Boolean?) {
+        set(context, TOP_KEY + key, if (value != null && value) {
+            "true"
+        } else {
+            "false"
+        })
+    }
 
-        fun setBool(context: Context, key: String, value: Boolean?) {
-            set(context, key, if (value != null && value) { "true" }else {"false"})
-        }
-
-        fun getBool(context: Context, key: String): Boolean {
-            return get(context, key).equals("true", ignoreCase = true)
-        }
+    fun getBool(context: Context, key: String): Boolean {
+        return get(context, TOP_KEY + key).equals("true", ignoreCase = true)
     }
 }
